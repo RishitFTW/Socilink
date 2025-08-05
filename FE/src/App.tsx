813aa2172065feed5a27c3468b3e57ca43aed03d
RishitFTW <rishupal631@gmail.com>
2025-08-05 16:37:22 +0530

    redirect to home

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -5,7 +5,7 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import 'react-toastify'
 import { ToastContainer } from 'react-toastify'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import { BrowserRouter,Routes, Route, Navigate } from 'react-router-dom'
 import SharedContent from './pages/SharedContent'
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
          <Route path="/signup" element={<Signup/>}/>
          <Route path="/" element={<Login/>}/>
          <Route path="/shared/:hash" element={<SharedContent/>} />
+         <Route path="*" element={<Navigate to="/" replace />} />
        </Routes>
       <ToastContainer position="top-center"/> 
    </BrowserRouter>
